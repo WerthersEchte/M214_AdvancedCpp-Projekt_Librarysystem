9bf8416eb5ffd94f0dc5afab3913ed3793d9631c
WerthersEchte <eike.petersen@student.fh-kiel.de>
2016-04-11 14:01:22 +0200

    added a few tests

--- tests/server/book_test.cpp
+++ tests/server/book_test.cpp
@@ -6,9 +6,20 @@
 
 #include "core/book.h"
 
+// This is a bad test, as it depends on position in execution
+BOOST_AUTO_TEST_CASE( Book_test_getID_bad ) {
+    std::string vTestTitle = "Die Abenteuer von Wurstmann";
+    library::Book vBook( vTestTitle );
+    BOOST_CHECK_EQUAL( 0, vBook.getId() );
+}
+
+BOOST_AUTO_TEST_CASE( Book_test_getID_unique ) {
+    library::Book vBook1( "first book" ), vBook2( "second book" );
+    BOOST_CHECK( vBook1.getId() != vBook2.getId() );
+}
+
 BOOST_AUTO_TEST_CASE( Book_test_getName ) {
     std::string vTestTitle = "Die Abenteuer von Wurstmann";
     library::Book vBook( vTestTitle );
     BOOST_CHECK_EQUAL( vTestTitle, vBook.getName() );
 }
-
