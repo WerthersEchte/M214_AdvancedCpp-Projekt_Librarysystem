97766b2606dc04913ae150c21954bbd56df965ae
WerthersEchte <eike.petersen@student.fh-kiel.de>
2016-04-11 08:51:45 +0200

    fixed server main.cpp fixed cmakelists added root-cmakelist

--- server/main.cpp
+++ server/main.cpp
@@ -1,16 +1,10 @@
-#include <QApplication>
-#include "gui/visiongui.h"
 #include <iostream>
 
 int main(int argc, char *argv[])
 {
 
-    QApplication a(argc, argv);
-    mrvision::VisionGui vVisionGui;
-	if( argc >= 2 ){
-		//vVisionGui.loadConfig( argv[1] );
-	}
-    vVisionGui.show();
+    std::cout << "This does nothing!" << std::endl;
+    
+    return 0;
 
-    return a.exec();
 }
