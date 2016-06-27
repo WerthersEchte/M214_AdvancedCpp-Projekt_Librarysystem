bcdf77c467c3eb67329aa26509f1b5df87dea4b8
OrangUtanKlaus <you@example.com>
2016-06-27 22:29:00 +0200

    Update edit book

--- client/gui/librarymaingui.cpp
+++ client/gui/librarymaingui.cpp
@@ -1,7 +1,9 @@
 #include "librarymaingui.h"
-#include "add+editbook.h"
+
 #include "definitions.h"
 #include <iostream>
+#include <boost/algorithm/string/split.hpp>
+#include <boost/algorithm/string/classification.hpp>
 
 namespace library{
 
@@ -45,11 +47,28 @@ MainGUI::MainGUI( QWidget *vParent ) : QMainWindow(vParent),
 
 
 
-	void MainGUI::editBook( bool vChecked ) {
-		if(mClient != nullptr){
-			std::string data = library::command::BOOK + static_cast<char>(library::Splitter::TYPE) + library::command::book::EDIT + static_cast<char>(library::Splitter::COMMAND);
+	void MainGUI::editBook(bool vChecked) {
+		if(mClient != nullptr) {
+			vAddEditBookDialog.setWindowTitle("Edit Book");
+			vAddEditBookDialog.show();
+			std::string data = library::command::BOOK + static_cast<char>(library::Splitter::TYPE) + library::command::book::GET + static_cast<char>(library::Splitter::COMMAND);
 			data += lEBook->text().toStdString();
 			mClient->write(data);
+			vAddEditBookDialog.exec();
+
+			if( vAddEditBookDialog.result() == QDialog::Accepted ){
+				if(mClient != nullptr){
+					std::string data = library::command::BOOK + static_cast<char>(library::Splitter::TYPE) + library::command::book::EDIT + static_cast<char>(library::Splitter::COMMAND);
+					data.append(vAddEditBookDialog.property("ID").toString().toStdString());
+					data += static_cast<char>(library::Splitter::COMMAND);
+					data += vAddEditBookDialog.lETitle->text().toStdString() + static_cast<char>(library::Splitter::COMMAND);
+					data +=vAddEditBookDialog.lEAuthor->text().toStdString() + static_cast<char>(library::Splitter::COMMAND);
+					data +=vAddEditBookDialog.lEPublisher->text().toStdString() + static_cast<char>(library::Splitter::COMMAND);
+					data +=vAddEditBookDialog.lEISBN->text().toStdString() + static_cast<char>(library::Splitter::COMMAND);
+					data +=vAddEditBookDialog.lEDate->text().toStdString();
+					mClient->write(data);
+				}
+			}
 		}
 	};
 
@@ -76,11 +95,11 @@ MainGUI::MainGUI( QWidget *vParent ) : QMainWindow(vParent),
 
 
 	void MainGUI::addBook( bool vChecked ) {
-		AddEditBook vAddEditBookDialog;
+
 		vAddEditBookDialog.setWindowTitle("Add Book");
 		vAddEditBookDialog.exec();
-		if( vAddEditBookDialog.result() == QDialog::Accepted ){
-			if(mClient != nullptr){
+		if( vAddEditBookDialog.result() == QDialog::Accepted ) {
+			if(mClient != nullptr) {
 				std::string data = library::command::BOOK + static_cast<char>(library::Splitter::TYPE) + library::command::book::ADD + static_cast<char>(library::Splitter::COMMAND);
 				data += vAddEditBookDialog.lETitle->text().toStdString() + static_cast<char>(library::Splitter::COMMAND);
 				data +=vAddEditBookDialog.lEAuthor->text().toStdString() + static_cast<char>(library::Splitter::COMMAND);
@@ -218,19 +237,33 @@ MainGUI::MainGUI( QWidget *vParent ) : QMainWindow(vParent),
 
 
 
-	void MainGUI::messageNetwork(QString aMessage ){
+	void MainGUI::messageNetwork(QString aMessage) {
 		pTERecive->appendPlainText(aMessage);
 	};
 
 
 
-	void MainGUI::messageBook(QString aMessage ){
-		pTEBookReceive->appendPlainText(aMessage);
+	void MainGUI::messageBook(QString aMessage) {
+		if(vAddEditBookDialog.isVisible()){
+			std::vector< std::string> vContentParts;
+			std::string content(aMessage.toStdString());
+			boost::split( vContentParts, content, boost::is_any_of(","));
+
+			if(vContentParts.size()>0) {
+				vAddEditBookDialog.setProperty("ID", std::stoi(vContentParts[0].substr(1, vContentParts[0].length()-2)));
+				vAddEditBookDialog.lETitle->setText(QString::fromStdString(vContentParts.size() >= 2 ? vContentParts[1].substr(1, vContentParts[1].length()-2) : ""));
+				vAddEditBookDialog.lEAuthor->setText(QString::fromStdString(vContentParts.size() >= 3 ? vContentParts[2].substr(1, vContentParts[2].length()-2) : ""));
+				vAddEditBookDialog.lEPublisher->setText(QString::fromStdString(vContentParts.size() >= 4 ? vContentParts[3].substr(1, vContentParts[3].length()-2) : ""));
+				vAddEditBookDialog.lEDate->setText(QString::fromStdString(vContentParts.size() >= 5 ? vContentParts[4].substr(1, vContentParts[4].length()-2) : ""));
+				vAddEditBookDialog.lEISBN->setText(QString::fromStdString(vContentParts.size() >= 6 ? vContentParts[5].substr(1, vContentParts[5].length()-2) : ""));
+			}
+		}
+		else pTEBookReceive->appendPlainText(aMessage);
 	};
 
 
 
-	void MainGUI::messageUser(QString aMessage ){
+	void MainGUI::messageUser(QString aMessage) {
 		pTEUser->appendPlainText(aMessage);
 	};
 }
